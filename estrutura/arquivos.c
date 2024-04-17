ff72dacf36aa9474f3d91c2e820eb685c66f66d5
4moriim <143743604+4moriim@users.noreply.github.com>
2024-04-17 17:25:14 -0300

    typedef Struct{   char nome[100];   int telefone[50];   char curso;   double N1, N2; } Alunos;

--- estrutura/arquivos.c
+++ estrutura/arquivos.c
@@ -2,8 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+
 int main(){
-  FILE * arquivo
+  FILE * arquivo;
   arquivo = fopen("DadosEntrada.csv", "r");
   if (arquivo == NULL) {
 printf("Erro ao abrir o arquivo.\n");
