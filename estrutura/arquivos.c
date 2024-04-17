20c31546922b78b9c3a745eec9bcadb9cf8d71f1
4moriim <143743604+4moriim@users.noreply.github.com>
2024-04-17 17:52:47 -0300

    Update arquivos.c

--- estrutura/arquivos.c
+++ estrutura/arquivos.c
@@ -2,11 +2,28 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef Struct{
+  char nome[100];
+  int numero_telefone[50];
+  char curso;
+  double N1;
+  double N2;
+}Alunos;
+
+double Calcular_Nota(double N1, double N2){
+  double resultado;
+  resultado = (N1 + N2) / 2;
+  return(resultado);
+}
+
 int main(){
-  FILE * arquivo
+  FILE * arquivo;
   arquivo = fopen("DadosEntrada.csv", "r");
   if (arquivo == NULL) {
 printf("Erro ao abrir o arquivo.\n");
 exit(1);
 }
+
+ Alunos alunos;
+  
 }
