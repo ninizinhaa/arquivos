0672d1c0b80f8093826f40e5f3b812feb3221401
4moriim <143743604+4moriim@users.noreply.github.com>
2024-04-21 12:24:24 -0300

    Update arquivos.c

--- estrutura/arquivos.c
+++ estrutura/arquivos.c
@@ -2,51 +2,55 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct {
+  char nome[100];
+  char telefone[50];
+  char curso[50];
+  float N1;
+  float N2;
+} Alunos;
+
+float Calcular_Nota(float N1, float N2){
+  float resultado;
+  resultado = (N1 + N2) / 2.0;
+  return(resultado);
+}
+  
+const char* Aprovado_Reprovado (float media){
+  if (media >= 7.0) {
+    return ("APROVADO");
+  }
+  else {
+    return ("REPROVADO");
+  }
+}
+
 int main(){
+  system("cls");
+  Alunos alunos[100];
+  int i = 0;
+
   FILE *entrada = fopen("DadosEntrada.csv", "r");
   FILE *saida = fopen("SituacaoFinal.csv", "w");
   
-if (entrada == NULL || saida == NULL) {
+  if (entrada == NULL || saida == NULL) {
         printf("Erro ao abrir os arquivos.\n");
         exit(1);
-      Alunos alunos[100];
-    int i = 0;
-    while (fscanf(entrada, "%s %s %f %f\n", alunos[i].nome, alunos[i].curso, &alunos[i].N1, &alunos[i].N2) != EOF) {
-        i++; }
-  
-     Separar_cursos(alunos, i);
-}
-
- Alunos alunos;
-  
-}
-
-typedef Struct{
-  char nome[100];
-  int numero_telefone[50];
-  char curso;
-  double N1;
-  double N2;
-}Alunos;
-
-double Calcular_Nota(double N1, double N2){
-  double resultado;
-  resultado = (N1 + N2) / 2;
-  return(resultado);
-  
-  if (resultado >= 7) {
-    printf("nome, "%.2f,"APROVADO");
-  }
-  else {
-    printf("nome, "%.2f,"REPROVADO");
-  }
-}
-
-void Separar_cursos (Alunos alunos[], int tamanho) {
-    for (int i = 0; i < tamanho; i++) {
-        printf("Curso: %s\n", alunos[i].curso);
-        printf("Aluno: %s, Notas: %.2f, %.2f\n", alunos[i].nome, alunos[i].N1, alunos[i].N2);
     }
-}
+    
 
+  while (fscanf(entrada, "%[^,],%[^,],%[^,],%f,%f\n", alunos[i].nome, alunos[i].telefone, alunos[i].curso, &alunos[i].N1, &alunos[i].N2) != EOF) {
+        i++; 
+    }
+
+  for (int j = 0; j < i; j++) {
+        float media = Calcular_Nota(alunos[j].N1, alunos[j].N2);
+        const char* situacao = Aprovado_Reprovado(media);
+        fprintf(saida, "%s, %.2f, %s\n", alunos[j].nome, media, situacao);
+    }
+
+  fclose(entrada);
+  fclose(saida);
+
+}
 
